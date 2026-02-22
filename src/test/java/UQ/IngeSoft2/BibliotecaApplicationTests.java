28b28323b814c72db6cef1c98dd6182fb10bbabd
sramirezsicarony <sramirezsicarony@gmail.com>
2026-02-22 12:16:45 -0500

    Primer commit para subir el proyecto con todas sus depnendencias

---
+++ src/test/java/UQ/IngeSoft2/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package UQ.IngeSoft2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
